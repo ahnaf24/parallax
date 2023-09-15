d8978c30a46b07a8b7351a5f52dcc1589fb1058f
Test User <ahnaf.shakil24@gmail.com>
2023-09-15 12:25:11 +0600

    l

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 
-let gameSpeed = 15;
+let gameSpeed = 10;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'layer-1.png';
@@ -16,17 +16,40 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
-let x = 0;
-let x2 = 2400;
+class Layer {
+    constructor (image, speedModifier) {
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 700;
+      this.x2 = this.width;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
+    }
+    update(){
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.width){
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if (this.x2 <= -this.width){
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x = Math.floor(this.x2 - this.speed);
+    }
+    draw(){
+       ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+       ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+const layer4 = new Layer(backgroundLayer4, 0.5);
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x, 0);
-    ctx.drawImage(backgroundLayer4, x2, 0);
-    if (x < -2400) x = 2400;
-    else x-= gameSpeed
-    if (x2 < -2400) x2 = 2400;
-    else x2-= gameSpeed
+    layer4.update();
+    layer4.draw();
     requestAnimationFrame(animate);
 };
 animate();
